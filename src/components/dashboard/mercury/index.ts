aa4dfbebdca1a9877e7e40f4e4a9be3124a3999d
David Pierce <david@scaleagilesolutions.com>
2026-02-19 03:59:49 -0500

    chore: remove dead AgentDriverPanel.tsx

--- src/components/dashboard/mercury/index.ts
+++ src/components/dashboard/mercury/index.ts
@@ -3,7 +3,5 @@
  */
 
 export { VoiceTrigger } from './VoiceTrigger'
-export { AgentDriverPanel } from './AgentDriverPanel'
-
 // Re-export types
 export type { AgentVoiceState, TranscriptEntry, ToolCallInfo } from '@/hooks/useSovereignAgentVoice'
